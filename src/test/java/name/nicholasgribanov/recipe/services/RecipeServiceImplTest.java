e26160a80813a6e25db1f0008a8dff2c578445e4
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-10-24 07:17:46 +0300

    Added jUnit test with Mockito for RecipeServiceImpl

---
+++ src/test/java/name/nicholasgribanov/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package name.nicholasgribanov.recipe.services;
+
+import name.nicholasgribanov.recipe.domain.Recipe;
+import name.nicholasgribanov.recipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipeHashSet = new HashSet<>();
+        recipeHashSet.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeHashSet);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
