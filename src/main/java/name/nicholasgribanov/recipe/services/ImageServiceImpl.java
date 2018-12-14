f933763cbbcef80e4f6db5a735563dfd117ea59e
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-12-14 08:32:45 +0300

    Added save Image in database

--- src/main/java/name/nicholasgribanov/recipe/services/ImageServiceImpl.java
+++ src/main/java/name/nicholasgribanov/recipe/services/ImageServiceImpl.java
@@ -1,14 +1,37 @@
 package name.nicholasgribanov.recipe.services;
 
 import lombok.extern.slf4j.Slf4j;
+import name.nicholasgribanov.recipe.domain.Recipe;
+import name.nicholasgribanov.recipe.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("save file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] bytes = new Byte[file.getBytes().length];
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                bytes[i++] = b;
+            }
+            recipe.setImage(bytes);
+
+            recipeRepository.save(recipe);
+        } catch (Exception e) {
+            log.error("Somthing brake into save image" + e.getLocalizedMessage());
+        }
     }
 }
